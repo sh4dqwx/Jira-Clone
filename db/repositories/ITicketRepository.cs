97cc7fd01172380ca90ea12d4676daab1162be2c
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-11-21 00:30:29 +0100

    Added ticketRepository

---
+++ db/repositories/ITicketRepository.cs
@@ -0,0 +1,16 @@
+﻿using JiraClone.db.dbmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JiraClone.db.repositories
+{
+    public interface ITicketRepository
+    {
+        public Ticket? GetTicketById(int id);
+        public List<Ticket> GetTicketsFromProject(Project project);
+        public void AddTicket(Ticket ticket);
+    }
+}
