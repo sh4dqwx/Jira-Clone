b1c4456feda7750db30870a7b03d7ce8c64fdeca
Bartosz Karolczak <82643@student.pb.edu.pl>
2023-11-27 15:59:59 +0100

    [JC-48] Added scrolling value inside Input object, adding and deleting characters in the middle

--- utils/consoleViewParts/options/Input.cs
+++ utils/consoleViewParts/options/Input.cs
@@ -2,6 +2,8 @@
 using System.Text;
 using System.Globalization;
 using System.Windows.Controls;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using System.Windows.Input;
 
 namespace JiraClone.utils.consoleViewParts.options
 {
@@ -10,44 +12,99 @@ namespace JiraClone.utils.consoleViewParts.options
         private readonly StringBuilder valueBuilder = new();
         private readonly ValidationRule? _validationRule;
         private bool _isPassword;
-
-        public string Value
-        {
-            get { return valueBuilder.ToString(); }
-            set
-            {
-                valueBuilder.Clear();
-                valueBuilder.Append(value);
-            }
-        }
-
-        public bool IsPassword { get => _isPassword; }
+        private int _inputStart;
+        private int _inputEnd;
+        private int _inputWidth;
+        private int _startIndex;
+        private int _endIndex;
 
         public Input(string name, bool isPassword = false, ValidationRule? validationRule = null) : base(name)
         {
+            Left = 0;
             Height = 5;
             Width = Constants.BUTTON_WIDTH;
 			_isPassword = isPassword;
 			_validationRule = validationRule;
+            _startIndex = 0;
+            _endIndex = 0;
 		}
 
         public override bool UseKey(ConsoleKeyInfo c)
         {
-            if (Console.CursorLeft < Left + Width - Constants.InputMargin - 1 && c.KeyChar >= 32 && c.KeyChar <= 127)
+            if (c.Key == ConsoleKey.LeftArrow)
             {
-                valueBuilder.Append(c.KeyChar);
-                if (_isPassword) Console.Write('*');
-                else Console.Write(c.KeyChar);
+                if (Console.CursorLeft > _inputStart)
+                {
+                    Console.CursorLeft--;
+                    return true;
+                }
+                else if (Console.CursorLeft == _inputStart && _startIndex > 0)
+                {
+                    _startIndex--;
+					PrintValue(modified: true);
+					return true;
+                }
+            }
+            else if (c.Key == ConsoleKey.RightArrow)
+            {
+                if (Console.CursorLeft < _inputEnd)
+                {
+					Console.CursorLeft++;
+                    return true;
+				}
+                else if (Console.CursorLeft == _inputEnd && Value.Length - _startIndex > _endIndex)
+                {
+                    _startIndex++;
+					PrintValue(modified: true);
+					return true;
+                } 
+            }
+            else if (c.KeyChar >= 32 && c.KeyChar <= 127)
+            {
+                int position = Console.CursorLeft - _inputStart + _startIndex;
+                valueBuilder.Insert(position, c.KeyChar);
+				if (Console.CursorLeft < _inputEnd && _startIndex == 0) Console.CursorLeft++;
+
+				if (_endIndex < _inputWidth - 1) _endIndex++;
+                else _startIndex++;
+                
+                PrintValue(modified: true);
                 return true;
             }
-            else if (Console.CursorLeft > Left + Constants.InputSpacer + Constants.InputMargin && c.Key == ConsoleKey.Backspace)
+            else if (Console.CursorLeft > _inputStart && c.Key == ConsoleKey.Backspace)
             {
-                valueBuilder.Remove(valueBuilder.Length - 1, 1);
-                Console.Write("\b \b");
+                int position = Console.CursorLeft - _inputStart + _startIndex;
+                valueBuilder.Remove(position - 1, 1);
+				if (Console.CursorLeft < _inputEnd || _startIndex == 0) Console.CursorLeft--;
+
+				if (Value.Length < _endIndex) _endIndex--;
+                else _startIndex--;
+
+                PrintValue(modified: true);
                 return true;
             }
             return false;
         }
+
+        private void PrintValue(bool modified = false)
+        {
+            int cursorLeft = Console.CursorLeft;
+            int cursorTop = Console.CursorTop;
+
+            Console.CursorVisible = false;
+
+			Console.SetCursorPosition(_inputStart, Top + 2);
+			for (int i = _inputStart; i < _inputEnd; i++) Console.Write(' ');
+
+            Console.SetCursorPosition(_inputStart, Top + 2);
+            if (_isPassword) Console.Write(new StringBuilder().Append('*', _endIndex - _startIndex));
+			else Console.Write(Value.Substring(_startIndex, _endIndex));
+
+			if (modified) Console.SetCursorPosition(cursorLeft, cursorTop);
+            else Console.SetCursorPosition(_inputStart + _endIndex, Top + 2);
+			
+            Console.CursorVisible = true;
+		}
 
         public override void Print()
         {
@@ -85,7 +142,7 @@ namespace JiraClone.utils.consoleViewParts.options
 
 			Console.SetCursorPosition(cursorLeft + Constants.InputSpacer + Constants.InputMargin, cursorTop);
 			Console.ForegroundColor = ConsoleColor.White;
-			Console.Write(_isPassword ? new StringBuilder().Append('*', valueBuilder.Length).ToString() : valueBuilder.ToString());
+            PrintValue();
             if (Selected) Console.CursorVisible = true;
 		}
 
@@ -113,5 +170,29 @@ namespace JiraClone.utils.consoleViewParts.options
             valueBuilder.Clear();
             Error = "";
         }
-    }
+
+		public override int Left
+        {
+            get => base.Left;
+            set
+            {
+                base.Left = value;
+				_inputStart = Left + Constants.InputSpacer + Constants.InputMargin;
+				_inputEnd = Left + Width - Constants.InputMargin - 2;
+				_inputWidth = _inputEnd - _inputStart + 1;
+			}
+        }
+
+		public string Value
+		{
+            get => valueBuilder.ToString();
+			set
+			{
+				valueBuilder.Clear();
+				valueBuilder.Append(value);
+			}
+		}
+
+		public bool IsPassword { get => _isPassword; }
+	}
 }
