a7e616d3acdb509fa69ab400bb1f6393f001bf8f
nitrazek <99548252+nitrazek@users.noreply.github.com>
2023-11-28 14:30:39 +0100

    [JC-60] Added validation for ticket type

---
+++ utils/validators/TicketTypeRule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace JiraClone.utils.validators
+{
+    public class TicketTypeRule: ValidationRule
+    {
+        private RequiredRule requiredRule = new RequiredRule();
+        private readonly string[] TYPES = { "FEATURE", "BUG" };
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            var requiredResult = requiredRule.Validate(value, cultureInfo);
+            if (requiredResult != ValidationResult.ValidResult) return requiredResult;
+
+            if (!TYPES.Contains((string)value))
+                return new ValidationResult(false, "Niepoprawny typ");
+
+            return ValidationResult.ValidResult;
+        }
+    }
+}
