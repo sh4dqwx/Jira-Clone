5f1578c5746d024d59edba89d0ba91db7a980daa
Bartosz Karolczak <82643@student.pb.edu.pl>
2023-11-29 21:12:09 +0100

    [JC-70] Added AddCommentView

---
+++ Migrations/20231129193933_test.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JiraClone.Migrations
+{
+    /// <inheritdoc />
+    public partial class test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTimestamp",
+                value: 1701286773L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreationTimestamp",
+                value: 1701246549L);
+        }
+    }
+}
